898576a86796eb813a353b5b35ce67dab1302770
ldliang93 <ldliang93@163.com>
2018-05-22 16:28:45 +0800

    practice_2

--- spec/practice_1/main_spec.js
+++ spec/practice_1/main_spec.js
@@ -7,13 +7,12 @@ chai.use(sinonChai);
 import Person from "../../src/practice_1/person.js";
 
 describe("Person", () => {
-    it("should have field name and age", () => {
+    iit("should have field name and age", () => {
         const person = new Person("Tom", 21);
         expect(person.name).to.equal("Tom");
         expect(person.age).to.equal(21);
     });
-
-    it("should have a method introduce, introduce person with name and age", () => {
+    iit("should have a method introduce, introduce person with name and age", () => {
         const person = new Person("Tom", 21);
         const introduce = person.introduce();
         expect(introduce).to.equal("My name is Tom. I am 21 years old.");
